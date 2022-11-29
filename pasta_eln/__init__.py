357d309e487f9b9251a3d947927d2aa1b384fd4e
Dr. Steffen Brinckmann <sbrinckm@gmail.com>
2022-11-29 11:58:00 +0100

    remove_clean3

--- pasta_eln/__init__.py
+++ pasta_eln/__init__.py
@@ -1,1 +1,4 @@
 """ EMPTY INIT FILE """
+from ../commit import get_version
+__version__ = get_version()
+del get_version
--- setup.py
+++ setup.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
-
-import sys
 from setuptools import setup
 import commit
 
 
 if __name__ == '__main__':
     setup(name='pasta_eln',
-          version=commit.get_version()
+          version=commit.get_version()[1:]
     )
