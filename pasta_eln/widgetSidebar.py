6a261e6a155715ea505de2f72445547342ddd2a3
Dr. Steffen Brinckmann <sbrinckm@gmail.com>
2023-02-06 10:43:21 +0100

    Towards a new sidebar

--- pasta_eln/widgetSidebar.py
+++ pasta_eln/widgetSidebar.py
@@ -1,5 +1,5 @@
 """ Sidebar widget that includes the navigation items """
-from PySide6.QtWidgets import QWidget, QVBoxLayout, QGridLayout    # pylint: disable=no-name-in-module
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QGridLayout, QTreeWidget, QTreeWidgetItem  # pylint: disable=no-name-in-module
 from anytree import PreOrderIter
 
 from .dialogConfig import Configuration
@@ -14,68 +14,94 @@ class Sidebar(QWidget):
       width = self.comm.backend.configuration['GUI']['sidebarWidth']
       self.setMinimumWidth(width)
       self.setMaximumWidth(width)
+    if not hasattr(comm.backend, 'db'):  #if no backend
+      configWindow = Configuration(comm.backend, 'setup')
+      configWindow.exec()
 
     # GUI elements
     mainL = QVBoxLayout()
     mainL.setContentsMargins(0,0,0,0)
     mainL.setSpacing(7)
     self.setLayout(mainL)
-    # storage of all projects
+    # storage of all project widgets and layouts
     self.widgets = {}
     self.layouts = {}
     self.widgetsHidden = {}
 
     if hasattr(comm.backend, 'db'):
-      # All projects
-      TextButton('All projects', self.btnDocType, mainL, 'x0/')
-      # Add other data types
-      dTypeW = QWidget()
-      dTypeL = QGridLayout(dTypeW)
-      dTypeL.setContentsMargins(0,0,0,0)
-      for idx, doctype in enumerate(comm.backend.db.dataLabels):
-        if doctype[0]!='x':
-          button = LetterButton(comm.backend.db.dataLabels[doctype], self.btnDocType, None, doctype+'/')
-          dTypeL.addWidget(button, int(idx/3), idx%3)
-      mainL.addWidget(dTypeW)
-
-      projectIDs = [i['id'] for i in comm.backend.db.getView('viewDocType/x0')]
-      currentID  = None
+      hierarchy = comm.backend.db.getView('viewDocType/x0')
+      projectIDs = [i['id'] for i in hierarchy]
       for projID in projectIDs:
-        hierarchy = comm.backend.db.getHierarchy(projID)
-        for node in PreOrderIter(hierarchy, maxlevel=2):
-          if node.docType[0][0]!='x':
-            continue
-          if node.docType[0]=='x0':
-            button = TextButton(node.name, self.btnProject, mainL, node.id+'/', style='margin-top: 30')
-            projectW = QWidget()
-            projectW.hide()
-            projectL = QVBoxLayout(projectW)
-            projectL.setContentsMargins(0,0,0,0)
-            currentID = node.id
-            # Add other data types
-            dTypeW = QWidget()
-            dTypeL = QGridLayout(dTypeW)
-            dTypeL.setContentsMargins(0,0,0,0)
-            for idx, doctype in enumerate(comm.backend.db.dataLabels):
-              if doctype[0]!='x':
-                button = LetterButton(comm.backend.db.dataLabels[doctype], self.btnDocType, None, doctype+'/'+currentID)
-                dTypeL.addWidget(button, int(idx/3), idx%3)
-            # create widgets
-            projectL.addWidget(dTypeW)
-            self.widgets[currentID] = projectW
-            self.layouts[currentID] = projectL
-            self.widgetsHidden[currentID] = True
-            mainL.addWidget(projectW)
-          if node.docType[0]=='x1':
-            button = TextButton(node.name, self.btnProject, self.layouts[currentID], currentID+'/'+node.id, \
-                                style='margin-left: 20')
+        #head: show project name as button
+        projectW = QWidget()
+        projectL = QVBoxLayout(projectW)
+        projectL.setContentsMargins(0,0,0,0)
+        try:
+          name     = [i for i in hierarchy if i['id']==projID][0]['value'][0]
+        except:
+          name     = 'ERROR OCCURRED'
+        btnProject = TextButton(name, self.btnProject, projectL, projID+'/')
+
+        # actions: scan, curate, ...
+        actionW = QWidget()
+        actionL = QGridLayout(actionW)
+        btnScan = TextButton('Scan', self.btnScan, None, projID)
+        actionL.addWidget(btnScan, 0,0)
+        btnCurate = TextButton('Curate', self.btnCurate, None, projID)
+        actionL.addWidget(btnCurate, 0,1)
+        projectL.addWidget(actionW)
+
+        # lists: view list of measurements, ... of this project
+        listW = QWidget()
+        listL = QGridLayout(listW)
+        for idx, doctype in enumerate(comm.backend.db.dataLabels):
+          if doctype[0]!='x':
+            button = LetterButton(comm.backend.db.dataLabels[doctype], self.btnDocType, None, doctype+'/'+projID, style='color: red')
+            listL.addWidget(button, int(idx/3), idx%3)
+        projectL.addWidget(listW)
+
+        # show folders as hierarchy
+        treeW = QTreeWidget()
+        treeW.setColumnCount(1)
+
+        projectL.addWidget(treeW)
+
+        # save changes
+        self.widgets[projID] = projectW
+        self.layouts[projID] = projectL
+        self.widgetsHidden[projID] = True
+        mainL.addWidget(projectW)
+
+
+
+        # hierarchy = comm.backend.db.getHierarchy(projID)
+        # for node in PreOrderIter(hierarchy, maxlevel=2):
+        #   if node.docType[0][0]!='x':
+        #     continue
+        #   if node.docType[0]=='x0':
+        #     button = TextButton(node.name, self.btnProject, mainL, node.id+'/', style='margin-top: 30')
+        #     currentID = node.id
+        #     # Add other data types
+        #     dTypeW = QWidget()
+        #     dTypeL = QGridLayout(dTypeW)
+        #     dTypeL.setContentsMargins(0,0,0,0)
+        #     for idx, doctype in enumerate(comm.backend.db.dataLabels):
+        #       if doctype[0]!='x':
+        #         button = LetterButton(comm.backend.db.dataLabels[doctype], self.btnDocType, None, doctype+'/'+currentID)
+        #         dTypeL.addWidget(button, int(idx/3), idx%3)
+        #     # create widgets
+        #     projectL.addWidget(dTypeW)
+        #     self.widgets[currentID] = projectW
+        #     self.layouts[currentID] = projectL
+        #     self.widgetsHidden[currentID] = True
+        #     mainL.addWidget(projectW)
+        #   if node.docType[0]=='x1':
+        #     button = TextButton(node.name, self.btnProject, self.layouts[currentID], currentID+'/'+node.id, \
+        #                         style='margin-left: 20')
 
     # Other buttons
     mainL.addStretch(1)
-    IconButton('fa.gear', self.btnConfig, mainL, backend=self.comm.backend)
-    if not hasattr(comm.backend, 'db'):  #if no backend
-      configWindow = Configuration(comm.backend, 'setup')
-      configWindow.exec()
+
 
 
   def btnConfig(self):
@@ -113,4 +139,13 @@ class Sidebar(QWidget):
         self.widgetsHidden[projID]=True
     else:
       self.comm.changeProject.emit(projID, item)
+    return
+
+  def btnScan(self):
+    """
+    """
+    return
+  def btnCurate(self):
+    """
+    """
     return
