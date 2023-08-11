b3b707439c60c36849e255ed0479bfe0a60eafb9
jmurugan-fzj <j.murugan@fz-juelich.de>
2023-08-11 13:42:31 +0200

    -Code Cleanup

--- tests/app-tests/test_gui_launch.py
+++ tests/app-tests/test_gui_launch.py
@@ -7,16 +7,16 @@ from pasta_eln.gui import main_gui
 
 @pytest.fixture(scope="module")
 def qtbot_session(qapp, request):
-    print("  SETUP qtbot")
+    print("Setting up QTBOT.....")
     result = QtBot(qapp)
     with capture_exceptions():
         yield result
-    print("  TEARDOWN qtbot")
+    print("Tearing down QTBOT.....")
 
 
 @pytest.fixture(scope="module")
 def gui(request):
-    print("  SETUP GUI")
+    print("Setting up GUI...")
 
     app, image_viewer = main_gui()
     qtbot = QtBot(app)
@@ -26,7 +26,7 @@ def gui(request):
 
 
 def test_app_launch(gui):
-    print("  beginning ")
+    print("Running test_app_launch....")
     app, imageViewer, qtbot = gui
     assert imageViewer.sidebar is not None, "Sidebar not loaded!"
     assert imageViewer.sidebar.widgetsList is not None, "Widgets not loaded!"
