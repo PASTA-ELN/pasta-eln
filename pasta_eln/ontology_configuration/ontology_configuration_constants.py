b6dc2cbd2d4ead3968fd680c3a9de0c351ce7fd5
jmurugan-fzj <j.murugan@fz-juelich.de>
2023-08-24 09:22:41 +0200

    - Modified the OntologyConfigurationForm to take ontology document directly as a parameter instead of db instance - Added exceptions and some more refactorings

---
+++ pasta_eln/ontology_configuration/ontology_configuration_constants.py
@@ -0,0 +1,16 @@
+#  PASTA-ELN and all its sub-parts are covered by the MIT license.
+#
+#  Copyright (c) 2023
+#
+#  Author: Jithu Murugan
+#  Filename: ontology_configuration_constants.py
+#
+#  You should have received a copy of the license with this file. Please refer the license file for more information.
+
+# Ontology Table view constants
+PROPS_TABLE_REQUIRED_COLUMN_INDEX = 4
+PROPS_TABLE_DELETE_COLUMN_INDEX = 6
+PROPS_TABLE_REORDER_COLUMN_INDEX = 7
+
+ATTACHMENT_TABLE_DELETE_COLUMN_INDEX = 2
+ATTACHMENT_TABLE_REORDER_COLUMN_INDEX = 3
