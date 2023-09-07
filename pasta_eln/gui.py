3b326a5f77bb1fe138bd35bf88bbece8543ad421
jmurugan-fzj <j.murugan@fz-juelich.de>
2023-09-07 16:26:19 +0200

    - Corrected PyLint, added new lines to the end of files

--- pasta_eln/gui.py
+++ pasta_eln/gui.py
@@ -1,92 +1,99 @@
 """ Graphical user interface includes all widgets """
-import os, logging, webbrowser, json, sys
+import json
+import logging
+import os
+import sys
+import webbrowser
 from enum import Enum
-from typing import Any, Optional, Union
 from pathlib import Path
-from PySide6.QtWidgets import QMainWindow, QApplication, QFileDialog, QScrollArea # pylint: disable=no-name-in-module
+from typing import Any, Union
+
 from PySide6.QtCore import Qt, Slot, QCoreApplication  # pylint: disable=no-name-in-module
 from PySide6.QtGui import QIcon, QPixmap, QShortcut  # pylint: disable=no-name-in-module
-from qt_material import apply_stylesheet  #of https://github.com/UN-GCPDS/qt-material
+from PySide6.QtWidgets import QMainWindow, QApplication, QFileDialog  # pylint: disable=no-name-in-module
+from qt_material import apply_stylesheet  # of https://github.com/UN-GCPDS/qt-material
 
 from pasta_eln import __version__
-from .backend import Backend
-from .guiCommunicate import Communicate
-from .GUI.sidebar import Sidebar
 from .GUI.body import Body
-from .GUI.form import Form
 from .GUI.config import Configuration
+from .GUI.form import Form
 from .GUI.projectGroup import ProjectGroup
-from .GUI.ontology import Ontology
-from .miscTools import updateExtractorList, restart
-from .guiStyle import Action, showMessage, widgetAndLayout, shortCuts
+from .GUI.sidebar import Sidebar
+from .backend import Backend
 from .fixedStringsJson import shortcuts
+from .guiCommunicate import Communicate
+from .guiStyle import Action, showMessage, widgetAndLayout, shortCuts
 from .inputOutput import exportELN, importELN
+from .miscTools import updateExtractorList, restart
+
 os.environ['QT_API'] = 'pyside6'
+
 
 # Subclass QMainWindow to customize your application's main window
 class MainWindow(QMainWindow):
   """ Graphical user interface includes all widgets """
+
   def __init__(self) -> None:
-    #global setting
+    # global setting
     super().__init__()
     venv = ' without venv' if sys.prefix == sys.base_prefix and 'CONDA_PREFIX' not in os.environ else ' in venv'
     self.setWindowTitle(f"PASTA-ELN {__version__}{venv}")
-    self.setWindowState(Qt.WindowMaximized) # type: ignore
-    resourcesDir = Path(__file__).parent/'Resources'
-    self.setWindowIcon(QIcon(QPixmap(resourcesDir/'Icons'/'favicon64.png')))
+    self.setWindowState(Qt.WindowMaximized)  # type: ignore
+    resourcesDir = Path(__file__).parent / 'Resources'
+    self.setWindowIcon(QIcon(QPixmap(resourcesDir / 'Icons' / 'favicon64.png')))
     self.backend = Backend()
     self.comm = Communicate(self.backend)
     self.comm.formDoc.connect(self.formDoc)
 
-    #Menubar
+    # Menubar
     menu = self.menuBar()
     projectMenu = menu.addMenu("&Project")
-    Action('&Export .eln',          self, [Command.EXPORT], projectMenu)
-    Action('&Import .eln',          self, [Command.IMPORT], projectMenu)
+    Action('&Export .eln', self, [Command.EXPORT], projectMenu)
+    Action('&Import .eln', self, [Command.IMPORT], projectMenu)
     projectMenu.addSeparator()
-    Action('&Exit',                 self, [Command.EXIT],   projectMenu)
+    Action('&Exit', self, [Command.EXIT], projectMenu)
 
     viewMenu = menu.addMenu("&Lists")
     if hasattr(self.backend, 'db'):
       for docType, docLabel in self.comm.backend.db.dataLabels.items():
-        if docType[0]=='x' and docType[1]!='0':
+        if docType[0] == 'x' and docType[1] != '0':
           continue
         shortcut = f"Ctrl+{shortCuts[docType]}" if docType in shortCuts else None
-        Action(docLabel,            self, [Command.VIEW, docType], viewMenu, shortcut=shortcut)
-        if docType=='x0':
+        Action(docLabel, self, [Command.VIEW, docType], viewMenu, shortcut=shortcut)
+        if docType == 'x0':
           viewMenu.addSeparator()
       viewMenu.addSeparator()
-      Action('&Tags',               self, [Command.VIEW, '_tags_'], viewMenu, shortcut='Ctrl+T')
-      Action('&Unidentified',       self, [Command.VIEW, '-'],      viewMenu, shortcut='Ctrl+U')
+      Action('&Tags', self, [Command.VIEW, '_tags_'], viewMenu, shortcut='Ctrl+T')
+      Action('&Unidentified', self, [Command.VIEW, '-'], viewMenu, shortcut='Ctrl+U')
 
     systemMenu = menu.addMenu("&System")
-    Action('&Project groups',       self, [Command.PROJECT_GROUP],    systemMenu)
+    Action('&Project groups', self, [Command.PROJECT_GROUP], systemMenu)
     changeProjectGroups = systemMenu.addMenu("&Change project group")
-    if hasattr(self.backend, 'configuration'):                       #not case in fresh install
+    if hasattr(self.backend, 'configuration'):  # not case in fresh install
       for name in self.backend.configuration['projectGroups'].keys():
-        Action(name,                self, [Command.CHANGE_PG, name], changeProjectGroups)
-    Action('&Syncronize',           self, [Command.SYNC],            systemMenu, shortcut='F5')
-    Action('&Questionaires',        self, [Command.ONTOLOGY],        systemMenu)
+        Action(name, self, [Command.CHANGE_PG, name], changeProjectGroups)
+    Action('&Syncronize', self, [Command.SYNC], systemMenu, shortcut='F5')
+    Action('&Questionaires', self, [Command.ONTOLOGY], systemMenu)
     systemMenu.addSeparator()
-    Action('&Test extraction from a file',   self, [Command.TEST1],  systemMenu)
-    Action('Test &selected item extraction', self, [Command.TEST2],  systemMenu, shortcut='F2')
-    Action('Update &Extractor list',         self, [Command.UPDATE], systemMenu)
+    Action('&Test extraction from a file', self, [Command.TEST1], systemMenu)
+    Action('Test &selected item extraction', self, [Command.TEST2], systemMenu, shortcut='F2')
+    Action('Update &Extractor list', self, [Command.UPDATE], systemMenu)
     systemMenu.addSeparator()
-    Action('&Configuration',         self, [Command.CONFIG],         systemMenu, shortcut='Ctrl+0')
+    Action('&Configuration', self, [Command.CONFIG], systemMenu, shortcut='Ctrl+0')
 
     helpMenu = menu.addMenu("&Help")
-    Action('&Website',               self, [Command.WEBSITE],        helpMenu)
-    Action('&Verify database',       self, [Command.VERIFY_DB],      helpMenu, shortcut='Ctrl+?')
-    Action('Shortcuts',              self, [Command.SHORTCUTS],      helpMenu)
+    Action('&Website', self, [Command.WEBSITE], helpMenu)
+    Action('&Verify database', self, [Command.VERIFY_DB], helpMenu, shortcut='Ctrl+?')
+    Action('Shortcuts', self, [Command.SHORTCUTS], helpMenu)
     helpMenu.addSeparator()
-    #shortcuts for advanced usage (user should not need)
-    QShortcut('F9', self, lambda : self.execute([Command.RESTART]))
+    # shortcuts for advanced usage (user should not need)
+    QShortcut('F9', self, lambda: self.execute([Command.RESTART]))
 
-    #GUI elements
+    # GUI elements
     mainWidget, mainLayout = widgetAndLayout('H')
-    self.setCentralWidget(mainWidget)      # Set the central widget of the Window
-    body = Body(self.comm)        #body with information
-    self.sidebar = Sidebar(self.comm)  #sidebar with buttons
+    self.setCentralWidget(mainWidget)  # Set the central widget of the Window
+    body = Body(self.comm)  # body with information
+    self.sidebar = Sidebar(self.comm)  # sidebar with buttons
     # sidebarScroll = QScrollArea()
     # sidebarScroll.setWidget(self.sidebar)
     # if hasattr(self.comm.backend, 'configuration'):
@@ -94,11 +101,10 @@ class MainWindow(QMainWindow):
     # mainLayout.addWidget(sidebarScroll)
     mainLayout.addWidget(self.sidebar)
     mainLayout.addWidget(body)
-    self.comm.changeTable.emit('x0','')
-
+    self.comm.changeTable.emit('x0', '')
 
   @Slot(str)
-  def formDoc(self, doc:dict[str,Any]) -> None:
+  def formDoc(self, doc: dict[str, Any]) -> None:
     """
     What happens when new/edit dialog is shown
 
@@ -106,19 +112,18 @@ class MainWindow(QMainWindow):
       doc (dict): document
     """
     if '_id' in doc:
-      logging.debug('gui:formdoc '+str(doc['_id']))
+      logging.debug('gui:formdoc ' + str(doc['_id']))
     elif '_ids' in doc:
-      logging.debug('gui:formdoc '+str(doc['_ids']))
+      logging.debug('gui:formdoc ' + str(doc['_ids']))
     else:
-      logging.debug('gui:formdoc of type '+str(doc['-type']))
+      logging.debug('gui:formdoc of type ' + str(doc['-type']))
     formWindow = Form(self.comm, doc)
     ret = formWindow.exec()
-    if ret==0:
+    if ret == 0:
       self.comm.stopSequentialEdit.emit()
     return
 
-
-  def execute(self,  command:list[Any]) -> None:
+  def execute(self, command: list[Any]) -> None:
     """
     action after clicking menu item
     """
@@ -127,38 +132,39 @@ class MainWindow(QMainWindow):
       if self.comm.projectID == '':
         showMessage(self, 'Error', 'You have to open a project to export', 'Warning')
         return
-      fileName = QFileDialog.getSaveFileName(self,'Save data into .eln file',str(Path.home()),'*.eln')[0]
+      fileName = QFileDialog.getSaveFileName(self, 'Save data into .eln file', str(Path.home()), '*.eln')[0]
       status = exportELN(self.comm.backend, self.comm.projectID, fileName)
       showMessage(self, 'Finished', status, 'Information')
     elif command[0] is Command.IMPORT:
-      fileName = QFileDialog.getOpenFileName(self,'Load data from .eln file',str(Path.home()),'*.eln')[0]
+      fileName = QFileDialog.getOpenFileName(self, 'Load data from .eln file', str(Path.home()), '*.eln')[0]
       status = importELN(self.comm.backend, fileName)
       showMessage(self, 'Finished', status, 'Information')
       self.comm.changeSidebar.emit('redraw')
-      self.comm.changeTable.emit('x0','')
+      self.comm.changeTable.emit('x0', '')
     elif command[0] is Command.EXIT:
       self.close()
-    #view menu
+    # view menu
     elif command[0] is Command.VIEW:
       self.comm.changeTable.emit(command[1], '')
-    #system menu
+    # system menu
     elif command[0] is Command.PROJECT_GROUP:
       dialog = ProjectGroup(self.comm)
       dialog.exec()
     elif command[0] is Command.CHANGE_PG:
       self.backend.configuration['defaultProjectGroup'] = command[1]
-      with open(Path.home()/'.pastaELN.json', 'w', encoding='utf-8') as fConf:
-        fConf.write(json.dumps(self.backend.configuration,indent=2))
+      with open(Path.home() / '.pastaELN.json', 'w', encoding='utf-8') as fConf:
+        fConf.write(json.dumps(self.backend.configuration, indent=2))
       restart()
     elif command[0] is Command.SYNC:
       report = self.comm.backend.replicateDB(progressBar=self.sidebar.progress)
       showMessage(self, 'Report of syncronization', report, style='QLabel {min-width: 450px}')
     elif command[0] is Command.ONTOLOGY:
-      showMessage(self, 'To be implemented','A possibility to change the questionaires / change the ontology is missing.')
+      showMessage(self, 'To be implemented',
+                  'A possibility to change the questionaires / change the ontology is missing.')
       # dialog = Ontology(self.comm.backend)
       # dialog.exec()
     elif command[0] is Command.TEST1:
-      fileName = QFileDialog.getOpenFileName(self,'Open file for extractor test',str(Path.home()),'*.*')[0]
+      fileName = QFileDialog.getOpenFileName(self, 'Open file for extractor test', str(Path.home()), '*.*')[0]
       report = self.comm.backend.testExtractor(fileName, outputStyle='html')
       showMessage(self, 'Report of extractor test', report)
     elif command[0] is Command.TEST2:
@@ -170,7 +176,7 @@ class MainWindow(QMainWindow):
     elif command[0] is Command.CONFIG:
       dialog = Configuration(self.comm)
       dialog.exec()
-    #remainder
+    # remainder
     elif command[0] is Command.WEBSITE:
       webbrowser.open('https://pasta-eln.github.io/pasta-eln/')
     elif command[0] is Command.VERIFY_DB:
@@ -182,8 +188,9 @@ class MainWindow(QMainWindow):
     elif command[0] is Command.RESTART:
       restart()
     else:
-      print("**ERROR gui menu unknown:",command)
+      print("**ERROR gui menu unknown:", command)
     return
+
 
 def main_gui() -> tuple[Union[QApplication, QCoreApplication, None], MainWindow]:
   """
@@ -218,24 +225,26 @@ def main_gui() -> tuple[Union[QApplication, QCoreApplication, None], MainWindow]
   logging.info('End PASTA GUI')
   return application, main_window
 
+
 class Command(Enum):
   """ Commands used in this file """
   EXPORT = 1
   IMPORT = 2
-  EXIT   = 3
-  VIEW   = 4
+  EXIT = 3
+  VIEW = 4
   PROJECT_GROUP = 5
-  CHANGE_PG     = 6
-  SYNC          = 7
-  ONTOLOGY      = 8
-  TEST1         = 9
-  TEST2         = 10
-  UPDATE        = 11
-  CONFIG        = 12
-  WEBSITE       = 13
-  VERIFY_DB     = 14
-  SHORTCUTS     = 15
-  RESTART       = 16
+  CHANGE_PG = 6
+  SYNC = 7
+  ONTOLOGY = 8
+  TEST1 = 9
+  TEST2 = 10
+  UPDATE = 11
+  CONFIG = 12
+  WEBSITE = 13
+  VERIFY_DB = 14
+  SHORTCUTS = 15
+  RESTART = 16
+
 
 # called by python3 -m pasta_eln.gui
 if __name__ == '__main__':
