2ec6195fa6b2bd354aba37e9d8a143c443503184
jmurugan-fzj <j.murugan@fz-juelich.de>
2023-09-07 17:21:38 +0200

    - Reverted fixedStringsJson.py to the old version

--- pasta_eln/fixedStringsJson.py
+++ pasta_eln/fixedStringsJson.py
@@ -1,245 +1,66 @@
 """ Long strings and dictionaries/JSON that would obfuscate code """
-#  PASTA-ELN and all its sub-parts are covered by the MIT license.
-#
-#  Copyright (c) 2023
-#
-#  Author: Jithu Murugan
-#  Filename: fixedStringsJson.py
-#
-#  You should have received a copy of the license with this file. Please refer the license file for more information.
-
 from typing import Any
 
-defaultOntology: dict[str, Any] = {
-  "-version": 2,
-  "_id": "-ontology-",
-  "_rev": "5-a3f3f6b96ba5839081fa8b2f745df64a",
-  "instrument": {
-    "attachments": [
-      {
-        "link": "device1",
-        "location": "Right side of the instrument"
-      }
-    ],
-    "label": "Instruments",
-    "link": "",
-    "prop": {
-      "default": [
-        {
-          "name": "-name",
-          "query": "What is the name / path?",
-          "required": "True"
-        },
-        {
-          "name": "comment",
-          "query": "#tags comments :field:value: e.g. #SOP_v1"
-        },
-        {
-          "name": "vendor",
-          "query": "Who is the vendor?"
-        }
-      ]
-    }
-  },
-  "measurement": {
-    "attachments": [],
-    "label": "Measurements",
-    "link": "",
-    "prop": {
-      "default": [
-        {
-          "name": "-name",
-          "query": "What is the file name?"
-        },
-        {
-          "name": "-tags"
-        },
-        {
-          "name": "comment",
-          "query": "#tags comments remarks :field:value:"
-        },
-        {
-          "name": "-type"
-        },
-        {
-          "name": "image"
-        },
-        {
-          "name": "#_curated"
-        },
-        {
-          "link": "http://url.com",
-          "list": "sample",
-          "name": "sample",
-          "query": "Which sample was used?",
-          "required": "True",
-          "unit": "m"
-        },
-        {
-          "list": "procedure",
-          "name": "procedure",
-          "query": "Which procedure was used?"
-        }
-      ]
-    }
-  },
-  "procedure": {
-    "attachments": [],
-    "label": "Procedures",
-    "link": "",
-    "prop": {
-      "default": [
-        {
-          "name": "-name",
-          "query": "What is the name / path?"
-        },
-        {
-          "name": "-tags"
-        },
-        {
-          "name": "comment",
-          "query": "#tags comments :field:value: e.g. #SOP_v1"
-        },
-        {
-          "name": "content",
-          "query": "What is procedure (Markdown possible; autofilled if file given)?"
-        }
-      ]
-    }
-  },
-  "sample": {
-    "attachments": [],
-    "label": "Samples",
-    "link": "",
-    "prop": {
-      "default": [
-        {
-          "name": "-name",
-          "query": "What is the name / identifier of the sample?"
-        },
-        {
-          "name": "chemistry",
-          "query": "What is its chemical composition?"
-        },
-        {
-          "name": "-tags"
-        },
-        {
-          "name": "comment",
-          "query": "#tags comments remarks :field:value:"
-        },
-        {
-          "name": "qrCode"
-        }
-      ]
-    }
-  },
-  "x0": {
-    "attachments": [],
-    "label": "Projects",
-    "link": "http://url.com",
-    "prop": {
-      "category1": [
-        {
-          "link": "http://url.com",
-          "name": "-name",
-          "query": "What is the name of the property?",
-          "required": "True",
-          "unit": ""
-        },
-        {
-          "link": "http://url.com",
-          "name": "sample1",
-          "query": "",
-          "required": "True",
-          "unit": "m"
-        }
-      ],
-      "default": [
-        {
-          "link": "http://url.com",
-          "name": "-name",
-          "query": "What is the name of the project?",
-          "required": "True"
-        },
-        {
-          "list": [
-            "active",
-            "paused",
-            "passive",
-            "finished"
-          ],
-          "name": "status",
-          "query": "What is the project status"
-        },
-        {
-          "name": "objective",
-          "query": "What is the objective?"
-        },
-        {
-          "name": "-tags"
-        },
-        {
-          "name": "comment",
-          "query": "#tags comments remarks :field:value:"
-        }
-      ]
-    }
-  },
-  "x1": {
-    "attachments": [],
-    "label": "Tasks",
-    "link": "",
-    "prop": {
-      "default": [
-        {
-          "name": "-name",
-          "query": "What is the name of task?"
-        },
-        {
-          "name": "comment",
-          "query": "#tags comments remarks :field:value:"
-        }
-      ]
-    }
-  },
-  "x2": {
-    "attachments": [],
-    "label": "Subtasks",
-    "link": "",
-    "prop": {
-      "default": [
-        {
-          "name": "-name",
-          "query": "What is the name of subtask?"
-        },
-        {
-          "name": "comment",
-          "query": "#tags comments remarks :field:value:"
-        }
-      ]
-    }
-  }
+defaultOntology:dict[str,Any] = {
+  "_id":"-ontology-",
+  "-version":2,
+
+  "x0": {"link":"", "label":"Projects", "prop":[
+    {"name":"-name",    "query":"What is the name of the project?"},
+    {"name":"status",   "query":"What is the project status", "list":["active","paused","passive","finished"]},
+    {"name":"objective","query":"What is the objective?"},
+    {"name":"-tags"},
+    {"name":"comment",  "query":"#tags comments remarks :field:value:"}
+  ]},
+  "x1": {"link":"", "label":"Tasks", "prop":[
+    {"name":"-name",    "query":"What is the name of task?"},
+    {"name":"comment",  "query":"#tags comments remarks :field:value:"}
+  ]},
+  "x2": {"link":"", "label":"Subtasks", "prop":[
+    {"name":"-name",    "query":"What is the name of subtask?"},
+    {"name":"comment",  "query":"#tags comments remarks :field:value:"}
+  ]},
+
+  "measurement": {"link":"", "label":"Measurements", "prop":[
+    {"name":"-name",       "query":"What is the file name?"},
+    {"name":"-tags"},
+    {"name":"comment",    "query":"#tags comments remarks :field:value:"},
+    {"name":"-type"},
+    {"name":"image"},
+    {"name":"#_curated"},
+    {"name":"sample",     "query":"Which sample was used?",     "list":"sample"},
+    {"name":"procedure",  "query":"Which procedure was used?",  "list":"procedure"}
+  ]},
+  "sample": {"link":"", "label":"Samples", "prop":[
+    {"name":"-name",       "query":"What is the name / identifier of the sample?"},
+    {"name":"chemistry",  "query":"What is its chemical composition?"},
+    {"name":"-tags"},
+    {"name":"comment",    "query":"#tags comments remarks :field:value:"},
+    {"name":"qrCode"}
+  ]},
+  "procedure": {"link":"", "label":"Procedures", "prop":[
+    {"name":"-name",       "query":"What is the name / path?"},
+    {"name":"-tags"},
+    {"name":"comment",    "query":"#tags comments :field:value: e.g. #SOP_v1"},
+    {"name":"content",    "query":"What is procedure (Markdown possible; autofilled if file given)?"}
+  ]},
+  "instrument": {"link":"", "label":"Instruments", "prop":[
+    {"name":"-name",       "query":"What is the name / path?"},
+    {"name":"comment",    "query":"#tags comments :field:value: e.g. #SOP_v1"},
+    {"name":"vendor",     "query":"Who is the vendor?"}
+  ]}
 }
 
-defaultOntologyNode: list[dict[str, str]] = [
-  {
-    "name": "-name",
-    "query": "What is the file name?"
-  },
-  {
-    "name": "-tags"
-  },
-  {
-    "name": "comment",
-    "query": "#tags comments remarks :field:value:"
-  },
-  {
-    "name": "-type"
-  }
-]
 
-defaultConfiguration: dict[str, Any] = {
+defaultOntologyNode:list[dict[str,str]] = [
+  {"name": "-name", "query": "What is the file name?"},
+  {"name": "-tags"},
+  {"name": "comment", "query": "#tags comments remarks :field:value:"},
+  {"name": "-type"}
+  ]
+
+
+defaultConfiguration:dict[str,Any] = {
   "defaultProjectGroup": "research",
   "userID": "$os.getlogin()$",
   "version": 0,
@@ -247,149 +68,35 @@ defaultConfiguration: dict[str, Any] = {
   "qrPrinter": {},
   "extractorDir": "$(Path(__file__).parent/'Extractors').as_posix()$",
   "extractors": {},
-  "authors": [
-    {
-      "first": "",
-      "last": "",
-      "title": "",
-      "email": "",
-      "orcid": "",
-      "organizations": [
-        {
-          "organization": "",
-          "rorid": ""
-        }
-      ]
-    }
-  ],
+  "authors": [{"first":"", "last":"", "title":"", "email":"", "orcid":"","organizations":[{"organization":"", "rorid":""}]}],
   "GUI": {},
   "projectGroups": {}
-}
+  }
+
 
 # level 1: type of property
 #   within each: array of 3: description, default, all_choices
-configurationGUI: dict[str, Any] = {
+configurationGUI:dict[str,Any] = {
   "general": {
-    "theme": [
-      "Theme",
-      "light_blue",
-      [
-        "dark_amber",
-        "dark_blue",
-        "dark_cyan",
-        "dark_lightgreen",
-        "dark_pink",
-        "dark_purple",
-        "dark_red",
-        "dark_teal",
-        "dark_yellow",
-        "light_amber",
-        "light_blue",
-        "light_cyan",
-        "light_cyan_500",
-        "light_lightgreen",
-        "light_pink",
-        "light_purple",
-        "light_red",
-        "light_teal",
-        "light_yellow",
-        "none"
-      ]
-    ],
-    "loggingLevel": [
-      "Logging level (more->less)",
-      "INFO",
-      [
-        "DEBUG",
-        "INFO",
-        "WARNING",
-        "ERROR"
-      ]
-    ]
+    "theme": ["Theme",
+              "light_blue",
+              ["dark_amber","dark_blue","dark_cyan","dark_lightgreen","dark_pink","dark_purple", "dark_red",\
+               "dark_teal","dark_yellow","light_amber","light_blue","light_cyan","light_cyan_500","light_lightgreen",\
+               "light_pink","light_purple","light_red","light_teal","light_yellow","none"]],
+    "loggingLevel": ["Logging level (more->less)","INFO",["DEBUG","INFO","WARNING","ERROR"]],
   },
-  "dimensions": {
-    "sidebarWidth": [
-      "Sidebar width",
-      280,
-      [
-        220,
-        280,
-        340
-      ]
-    ],
-    "maxTableColumnWidth": [
-      "Maximum column width in tables",
-      400,
-      [
-        300,
-        400,
-        500,
-        600
-      ]
-    ],
-    "imageSizeDetails": [
-      "Image size in details view and form",
-      600,
-      [
-        300,
-        400,
-        500,
-        600
-      ]
-    ],
-    "imageWidthProject": [
-      "Image width in project view",
-      300,
-      [
-        200,
-        250,
-        300,
-        350,
-        400
-      ]
-    ],
-    "maxProjectLeafHeight": [
-      "Maximum height of item in project view",
-      250,
-      [
-        200,
-        250,
-        300,
-        400
-      ]
-    ],
-    "widthContent": [
-      "Width of procedures in project view",
-      600,
-      [
-        400,
-        500,
-        600,
-        700
-      ]
-    ],
-    "docTypeOffset": [
-      "Offset of document type in project view",
-      500,
-      [
-        400,
-        500,
-        600,
-        700
-      ]
-    ],
-    "frameSize": [
-      "Frame width around items in project view",
-      6,
-      [
-        4,
-        6,
-        8,
-        10
-      ]
-    ]
+  "dimensions":{
+    "sidebarWidth": ["Sidebar width", 280, [220, 280, 340]],
+    "maxTableColumnWidth": ["Maximum column width in tables", 400, [300, 400, 500, 600]],
+    "imageSizeDetails": ["Image size in details view and form", 600, [300, 400, 500, 600]],
+    "imageWidthProject": ["Image width in project view", 300, [200, 250, 300, 350, 400]],
+    "maxProjectLeafHeight": ["Maximum height of item in project view", 250, [200, 250, 300, 400]],
+    "widthContent": ["Width of procedures in project view", 600, [400, 500, 600, 700]],
+    "docTypeOffset": ["Offset of document type in project view", 500, [400, 500, 600, 700]],
+    "frameSize": ["Frame width around items in project view", 6, [4, 6, 8, 10]],
   }
 }
+
 
 setupTextLinux = """
 ### Welcome to the PASTA-ELN setup for Linux
@@ -402,6 +109,7 @@ This setup will analyse and (possibly) correct these items.
 
 If the installation is successful, manually and permanently remove the 'pastaELN.log' logfile that is in your home-directory.
 """
+
 
 setupTextWindows = """
 ### Welcome to the PASTA-ELN setup for Windows
@@ -469,6 +177,7 @@ This step helps to verify the installation and provides an helpful example for n
 This step usually takes up to 1min, so please be patient.
 """
 
+
 shortcuts = """
 Ctrl+Space: List projects
 Ctrl+M: List measurements
@@ -483,6 +192,7 @@ F9: Restart
 Ctrl+?: Verify database integrity
 Ctrl+0: Configuration
 """
+
 
 tableHeaderHelp = """
 <h4>You can add custom rows via bottom text area.</h4>
