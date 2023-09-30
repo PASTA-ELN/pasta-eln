ecd8af563aaa7082fb8419dee2e95cd942a91583
Steffen Brinckmann <sbrinckm@gmail.com>
2023-09-30 09:55:17 +0200

    adopt pre-push hook; update extractors

--- pasta_eln/Extractors/extractor_csv.py
+++ pasta_eln/Extractors/extractor_csv.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 def use(filePath, recipe='', saveFileName=None):
   """
   Args:
-    filePath (string): full path file name
+    filePath (Path): full path file name
     recipe (string): supplied to guide recipes
                      recipe is / separated hierarchical elements parent->child
     saveFileName (string): if given, save the image to this file-name
@@ -122,4 +122,4 @@ def use(filePath, recipe='', saveFileName=None):
   #other datatypes follow here
   #...
   #final return if nothing successful
-  #return {}
+  #return {'recipe': '-', 'metaVendor':{}, 'metaUser':{}}
