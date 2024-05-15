8c7aaa528dabe684d5b5192c66df58e2ff5e9b0e
Steffen Brinckmann <sbrinckm@gmail.com>
2024-05-15 08:49:59 +0200

    Followed suggestions of JM

--- pasta_eln/gui.py
+++ pasta_eln/gui.py
@@ -41,7 +41,6 @@ class MainWindow(QMainWindow):
     super().__init__()
     venv = ' without venv' if sys.prefix == sys.base_prefix and 'CONDA_PREFIX' not in os.environ else ' in venv'
     self.setWindowTitle(f"PASTA-ELN {__version__}{venv}")
-    # self.setWindowState(Qt.WindowMaximized)  # type: ignore
     resourcesDir = Path(__file__).parent / 'Resources'
     self.setWindowIcon(QIcon(QPixmap(resourcesDir / 'Icons' / 'favicon64.png')))
     self.backend = Backend()
