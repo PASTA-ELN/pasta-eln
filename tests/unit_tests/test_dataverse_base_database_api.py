f32ec587d87a2190784d9ab6994aee971a7fe014
jmurugan-fzj <j.murugan@fz-juelich.de>
2024-06-18 17:20:28 +0200

    - Code reformatting.

--- tests/unit_tests/test_dataverse_base_database_api.py
+++ tests/unit_tests/test_dataverse_base_database_api.py
@@ -114,41 +114,6 @@ class TestDataverseBaseDatabaseApi:
       mock_logger.assert_called_once_with('pasta_eln.dataverse.base_database_api.BaseDatabaseAPI')
       assert instance.logger == mock_logger.return_value
 
-  # @pytest.mark.parametrize("config, db_name, expected_username, expected_password, test_id", [
-  #   ({"projectGroups": {"test_db": {"local": {"user": "test_user", "password": "test_pass"}}}}, "test_db", "test_user",
-  #    "test_pass", "success_path"),
-  #   ({"projectGroups": {"test_db": {"local": {"user": "user1", "password": "pass1"}}}}, "test_db", "user1", "pass1",
-  #    "alternate_credentials"),
-  #   # Add more test cases for different realistic values
-  # ])
-  # def test_set_username_password_success_path(self, mocker, mock_database_api, config, db_name, expected_username,
-  #                                             expected_password, test_id):
-  #   # Arrange
-  #   mock_database_api.dataverse_db_name = db_name
-  #
-  #   # Act
-  #   mock_database_api.set_username_password(config)
-  #
-  #   # Assert
-  #   assert mock_database_api.username == expected_username, f"Test ID: {test_id} - Expected username to be {expected_username}"
-  #   assert mock_database_api.password == expected_password, f"Test ID: {test_id} - Expected password to be {expected_password}"
-
-  # @pytest.mark.parametrize("config, db_name, expected_exception, test_id", [
-  #   ({}, "test_db", ConfigError, "empty_config"),
-  #   ({"projectGroups": {}}, "test_db", ConfigError, "missing_project_groups"),
-  #   ({"projectGroups": {"test_db": {}}}, "test_db", ConfigError, "missing_local_info"),
-  #   ({"projectGroups": {"test_db": {"local": {}}}}, "test_db", ConfigError, "missing_user_password"),
-  #   # Add more test cases for different error scenarios
-  # ])
-  # def test_set_username_password_error_cases(self, mocker, mock_database_api, config, db_name, expected_exception,
-  #                                            test_id):
-  #   # Arrange
-  #   mock_database_api.dataverse_db_name = db_name
-  #
-  #   # Act & Assert
-  #   with pytest.raises(expected_exception) as exc_info:
-  #     mock_database_api.set_username_password(config)
-  #   assert exc_info, f"Test ID: {test_id} - Expected exception {expected_exception.__name__} was not raised"
   @pytest.mark.parametrize(
     "db_name, client_create_db_side_effect, expected_warning, expected_db_name",
     [
