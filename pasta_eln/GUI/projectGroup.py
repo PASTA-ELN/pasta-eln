95a5ac0b12e7a1a44ace45a1a0900db5c2270f95
Steffen Brinckmann <sbrinckm@gmail.com>
2024-11-26 08:36:13 +0100

    Finished projectGroups

--- pasta_eln/GUI/projectGroup.py
+++ pasta_eln/GUI/projectGroup.py
@@ -41,9 +41,8 @@ class ProjectGroup(QDialog):
     self.selectGroup.currentTextChanged.connect(self.changeProjectGroup)
     self.formL.addWidget(self.selectGroup, 0, 0)
     self.groupTextField = QLineEdit()
-    self.groupTextField.hide()
     self.groupTextField.setValidator(QRegularExpressionValidator("\\w{3,}"))
-    self.formL.addWidget(self.selectGroup, 0, 1)
+    self.comboboxActive = True
 
     newButton = IconButton('fa5s.plus',    self, [Command.NEW], tooltip='New project group')
     self.formL.addWidget(newButton, 0, 2)
@@ -56,7 +55,7 @@ class ProjectGroup(QDialog):
     self.formL.addWidget(row1Button, 1, 3)
 
     self.addOnLabel = QLabel('addon')
-    self.formL.addWidget(self.addOnLabel, 3, 0)
+    self.formL.addWidget(self.addOnLabel, 2, 0)
     row2Button = IconButton('fa5.edit',   self, [Command.CHANGE_ADDON], tooltip='Edit add-on path')
     self.formL.addWidget(row2Button, 2, 3)
 
@@ -124,14 +123,15 @@ class ProjectGroup(QDialog):
     Args:
       command (list): list of commands
     """
-    if self.selectGroup.isHidden():  #first button press after entering a new group name
+    if not self.comboboxActive:  #first button press after entering a new group name
       newKey = self.groupTextField.text()
       self.selectGroup.addItem(newKey)
       self.selectGroup.setCurrentText(newKey)
       self.configuration['projectGroups'][newKey] = self.emptyConfig
+      self.formL.removeWidget(self.groupTextField)
       self.formL.addWidget(self.selectGroup, 0,0)
-      self.groupTextField.setHidden(True)
-      self.selectGroup.setHidden(False)
+      self.groupTextField.hide()
+      self.comboboxActive = True
     key = self.selectGroup.currentText()
     if not key:
       return
@@ -166,7 +166,9 @@ class ProjectGroup(QDialog):
 
     elif command[0] is Command.TEST_SERVER:
       headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-      url = config['remote']['url']
+      url     = self.serverLabel.text().strip()
+      if not url:
+        return
       if not url.endswith('/'):
         url += '/'
       if not url.endswith('api/v2/'):
@@ -174,19 +176,22 @@ class ProjectGroup(QDialog):
       if not url.startswith('https'):
         url = 'https://' + url
       config['remote']['url'] = url
+      self.serverLabel.setText(url)
       try:
         requests.get(f'{url}info', headers=headers, verify=True, timeout=60)
-        self.row3Button.setStyleSheet('color: #00FF00')
+        self.row3Button.setStyleSheet('background: #00FF00')
       except Exception:
         showMessage(self, 'Error', 'Wrong server address')
-        self.row3Button.setStyleSheet('color: #FF0000')
+        self.row3Button.setStyleSheet('background: #FF0000')
 
     elif command[0] is Command.TEST_API_HELP:
+      link = f'OR go to {config["remote"]["url"][:-7]}ucp.php?tab=4\n\n' if config["remote"].get("url",'') else ''
       showMessage(self, 'Help', '### How to get an api key to access the server:\nOn the elabFTW server:\n1. go to the USER SYMBOL\n2. User-(Control) panel\n3. API KEYS\n\n'\
-                  f'OR go to {config["remote"]["url"][:-7]}ucp.php?tab=4\n\n'\
+                  f'{link}'\
                   '1. Specify a name: e.g. "pasta_eln"\n2. change the permissions to "Read/Write"\n3. click "Generate new API key"\n\nCopy-paste that key into the text box on the right-hand-side')
 
     elif command[0] is Command.TEST_APIKEY:
+      config['remote']['key'] = self.apiKeyLabel.toPlainText().strip()
       headers = {'Content-type': 'application/json', 'Accept': 'text/plain', 'Authorization':config['remote']['key']}
       url = config['remote']['url']
       response = requests.get(f'{url}info', headers=headers, verify=True, timeout=60)
@@ -194,10 +199,10 @@ class ProjectGroup(QDialog):
         elabVersion = int(json.loads(response.content.decode('utf-8')).get('elabftw_version','0.0.0').split('.')[0])
         if elabVersion<5:
           showMessage(self, 'Error', 'Old elabFTW server installation')
-        self.row4Button2.setStyleSheet('color: #00FF00')
+        self.row4Button2.setStyleSheet('background: #00FF00')
       else:
         showMessage(self, 'Error', 'Wrong API address')
-        self.row4Button2.setStyleSheet('color: #FF0000')
+        self.row4Button2.setStyleSheet('background: #FF0000')
 
     elif command[0] is Command.CREATE_QRCODE:
       text = json.dumps(config['remote'])
@@ -206,12 +211,14 @@ class ProjectGroup(QDialog):
       self.image.setPixmap(pixmap)
 
     elif command[0] is Command.NEW:
-      self.selectGroup.hide()
+      self.formL.removeWidget(self.selectGroup)
+      self.formL.addWidget(self.groupTextField, 0, 0)
       self.directoryLabel.setText('Data directory: ')
       self.addOnLabel.setText('Add on directory: ')
       self.serverLabel.setText('')
       self.apiKeyLabel.setText('')
       self.image.setPixmap(QPixmap())
+      self.comboboxActive = False
 
     elif command[0] is Command.DEL:
       del self.configuration['projectGroups'][key]
@@ -220,6 +227,7 @@ class ProjectGroup(QDialog):
 
     else:
       print("Got some button, without definition", command)
+    print(json.dumps(self.configuration['projectGroups'], indent=2))
     return
 
 
