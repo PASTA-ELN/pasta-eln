516f1a97ffdd79b58805649e5315473014e6049b
Steffen Brinckmann <sbrinckm@gmail.com>
2025-04-01 16:31:59 +0200

    mypy et al.

--- pasta_eln/Resources/ExampleWorkflows/workplan.py
+++ pasta_eln/Resources/ExampleWorkflows/workplan.py
@@ -1,11 +1,13 @@
 # head of workflow: always the same
 from urllib.parse import urlparse
-from common_workflow_description.common_workflow_description import Storage, Sample, step
+from common_workflow_description.common_workflow_description import Sample, Storage, step
+
 try:
     from pyiron_workflow import Workflow
 except ImportError:
     from common_workflow_description.common_workflow_description import Workflow
-from analysis_steps import plot_curves, calc_YoungsModulus
+
+from analysis_steps import calc_YoungsModulus, plot_curves
 
 # start code
 wf = Workflow('Sandia Fracture Challenge 3', automate_execution=False)         # name
@@ -30,6 +32,6 @@ wf.starting_nodes = [wf.step1]
 
 # footer, always the same
 print('Output:\n  ','\n   '.join([f"{k}: {v}" for k, v in list(wf.outputs.to_value_dict().items())]))
-wf.draw().render(filename="io_demo_2", format="png", cleanup=True) #plot to file
+wf.draw().render(filename='io_demo_2', format='png', cleanup=True) #plot to file
 
 # {"type":"common-workflow-description", "version":1.0, "shasum":"d5d8342b266851e28919e69039918d2c2a9ca4f2"}
